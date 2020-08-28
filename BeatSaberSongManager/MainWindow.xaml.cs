8844a0a9364866f2a88f4aa939f0a48ae3c08c95
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2020-08-28 12:35:24 +0200

    Added user controls. Started to make the UI

--- BeatSaberSongManager/MainWindow.xaml.cs
+++ BeatSaberSongManager/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using BeatSaberSongManager.UserControls;
+using MahApps.Metro.Controls;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,11 +20,33 @@ namespace BeatSaberSongManager
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : MetroWindow
     {
+        private readonly BeatmapLocalUserControl localUserControl;
+        private readonly BeatmapOnlineUserControl onlineUserControl;
+
         public MainWindow()
         {
             InitializeComponent();
+            localUserControl = new BeatmapLocalUserControl();
+            onlineUserControl = new BeatmapOnlineUserControl();
+
+            buttonLocal.IsChecked = true;
+        }
+
+        private void ButtonLocal_Checked(object sender, RoutedEventArgs e)
+        {
+            userControlMain.Content = localUserControl;
+        }
+
+        private void ButtonOnline_Checked(object sender, RoutedEventArgs e)
+        {
+            userControlMain.Content = onlineUserControl;
+        }
+
+        private void ButtonSettings_Checked(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ BeatSaberSongManager/UserControls/BeatmapOnlineUserControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace BeatSaberSongManager.UserControls
+{
+    /// <summary>
+    /// Interaction logic for BeatmapOnlineUserControl.xaml
+    /// </summary>
+    public partial class BeatmapOnlineUserControl : UserControl
+    {
+        public BeatmapOnlineUserControl()
+        {
+            InitializeComponent();
+        }
+    }
+}

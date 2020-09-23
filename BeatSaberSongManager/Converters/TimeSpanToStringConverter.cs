349da04af086a29ea8017918159928b7488bb110
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2020-09-23 09:42:12 +0200

    Added song duration

---
+++ BeatSaberSongManager/Converters/TimeSpanToStringConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows.Data;
+
+namespace BeatSaberSongManager.Converters
+{
+    public class TimeSpanToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is TimeSpan timeSpan)
+            {
+                if (timeSpan.Hours > 0)
+                    return timeSpan.ToString(@"hh\:mm\:ss", CultureInfo.CurrentCulture);
+                else
+                    return timeSpan.ToString(@"mm\:ss", CultureInfo.CurrentCulture);
+            }
+
+            return null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

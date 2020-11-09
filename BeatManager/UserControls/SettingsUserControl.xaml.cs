70186fe6e195b54c31b0ed7256d32d48afed5be5
Casper Bastholm <42714453+casperb123@users.noreply.github.com>
2020-11-09 13:31:32 +0100

    Changed restart as admin button

--- BeatManager/UserControls/SettingsUserControl.xaml.cs
+++ BeatManager/UserControls/SettingsUserControl.xaml.cs
@@ -72,11 +72,9 @@ namespace BeatManager.UserControls
             await ViewModel.ToggleOneClick(OneClickType.ModelSaber, Settings.CurrentSettings.ModelSaber.OneClickInstaller);
         }
 
-        private async void ButtonRestartAsAdmin_Click(object sender, RoutedEventArgs e)
+        private void ButtonRestartAsAdmin_Click(object sender, RoutedEventArgs e)
         {
-            MessageDialogResult result = await ViewModel.MainWindow.ShowMessageAsync("OneClick installer", "To enable OneClick installer you need to run the application as administrator. Would you like to restart the application as administrator?", MessageDialogStyle.AffirmativeAndNegative);
-            if (result == MessageDialogResult.Affirmative)
-                ViewModel.RestartAsAdmin();
+            ViewModel.RestartAsAdmin();
         }
     }
 }
